17462f9e7a6dced6492dd77c64f30866955b6714
wSTONE80085 <wyatt.stone@stu.daviess.kyschools.us>
2025-04-07 14:32:24 +0000

    done

--- Trio.java
+++ Trio.java
@@ -1,1 +1,12 @@
 //Write your answer here
+public class Trio implements MenuItem
+    private String name;
+    private Double price;
+
+    private sandwhich;
+    private salad;
+    private drink;
+
+    public Trio(sandwhich, salad, drink)
+    MenuItem[] items = {sandwhich, salad, drink}
+    
